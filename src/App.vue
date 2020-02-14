c94225a0ea10960712c7e1ba3473f89da779af4a
James <417224286@qq.con>
2020-02-14 22:39:47 +0800

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Tabbar>
+      <TabbarItem v-for="(item, index) in tabbarItems" :key="index" :text="item.name" :img-url="item.url" :img-url-active="item.urlActive" color-active="blue"/>
+    </Tabbar>
+  </div>
+</template>
+
+<script>
+  import Tabbar from './components/tabbar/Tabbar'
+  import TabbarItem from './components/tabbar/TabbarItem'
+  export default {
+    name: 'App',
+    components: {
+      Tabbar,
+      TabbarItem
+    },
+    data(){
+      return {
+        tabbarItems: [
+          {name: '首页', url: require('./assets/img/home.svg'), urlActive: require('./assets/img/home-active.svg')},
+          {name: '分类', url: require('./assets/img/category.svg'), urlActive: require('./assets/img/category-active.svg')},
+          {name: '购物车', url: require('./assets/img/cart.svg'), urlActive: require('./assets/img/cart-active.svg')},
+          {name: '我的', url: require('./assets/img/profile.svg'), urlActive: require('./assets/img/profile-active.svg')},
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  @import './assets/css/base.css';
+</style>
---
+++ src/components/tabbar/TabbarItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="tabbarItem" @click=" isActive = !isActive" :class="{active: isActive}">
+    <img :src="url"/>
+    {{text}}
+  </div>
+</template>
+<script>
+  export default {
+    props: ['imgUrl', 'imgUrlActive', 'text', {
+        'colorActive': {
+          type: String,
+          default: 'red'
+        }
+      }
+    ],
+    data(){
+      return {
+        isActive: false
+      }
+    },
+    computed: {
+      url(){
+        return this.isActive ? this.imgUrlActive : this.imgUrl
+      }
+    }
+  }
+</script>
+<style>
+  #tabbarItem{
+    height: 49px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-grow: 1
+  }
+  img{
+    width: 24px;
+    height: 24px;
+  }
+  .active{
+    color: #d4237a
+  }
+</style>
